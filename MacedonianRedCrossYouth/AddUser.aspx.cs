0e1a0d7f39277fc522ef50495f207ecd7c077451
Kristijan Milanov <kristijanmilanov@gmail.com>
2014-06-14 21:34:28 +0200

    AddUser
    AddUser webform

---
+++ MacedonianRedCrossYouth/AddUser.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MacedonianRedCrossYouth
+{
+    public partial class AddUser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
