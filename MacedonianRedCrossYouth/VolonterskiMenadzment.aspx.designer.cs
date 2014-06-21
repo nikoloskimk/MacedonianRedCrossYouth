e93591e434327db25356d1a148db93410f6708ac
Filip Nikoloski <fini993@yahoo.com>
2014-06-21 18:24:50 +0200

    Volonterski menadzment
    Volonterski menadzment

--- MacedonianRedCrossYouth/VolonterskiMenadzment.aspx.designer.cs
+++ MacedonianRedCrossYouth/VolonterskiMenadzment.aspx.designer.cs
@@ -29,5 +29,23 @@ namespace MacedonianRedCrossYouth {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvVolonteri;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// gvClenovi control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvClenovi;
     }
 }
